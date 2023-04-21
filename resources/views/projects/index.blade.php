f17cde235730720a6bdcbf949e8f74b2a67ccc70
eSSeGH <ste.virgadaula@gmail.com>
2023-04-21 15:07:36 +0200

    CRUD project views added

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,5 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-    project index
+<main>
+
+<div class="projects-list">
+
+    <div class="container">
+
+        <table class="table main-table">
+            <thead>
+                <tr>          
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Nome Cliente</th>
+                    <th scope="col">Telefono Cliente</th>
+                    <th scope="col">Opzioni</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                @foreach($projects as $project)
+                <tr>
+                    <td>
+                        {{ $project->title }} <br>
+                        <a href="{{ route('projects.show', $project->id) }}">Vai al progetto</a>
+                    </td>
+                    <td>{{ $project->description }}</td>                   
+                    <td>{{ $project->client_name }}</td>
+                    <td>{{ $project->client_tel }}</td>
+                    <td class="btn-td" >
+                        <a href="{{ route('projects.edit', $project) }}" class="edit-btn btn btn-secondary btn-sm mb-1">EDIT</a>
+                        <form class="mb-auto" action="{{ route('projects.destroy', $project) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit"  class="delete-btn btn btn-danger btn-sm" value="DELETE">
+                        </form>
+                        
+                    </td>
+                </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </div>
+
+</div>
+
+</main>
 @endsection
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    project show
+<main>
+
+<div class="container">
+
+    <div class="card mx-auto mt-5">
+        <div class="card-body">
+            <h3 class="card-title">{{$project['title']}}</h5>
+            <h4 class="card-text">Descrizione: 
+                <p style="font-size: 0.8rem;">{{ $project['description']}}</p>
+            </h4>
+            <h4 class="card-text">Prezzo: 
+                <span style="font-size: 0.8rem;">{{ $project['client_name'] }} $</span>
+            </h4>
+            <h4 class="card-text">Serie: 
+                <span style="font-size: 0.8rem;">{{ $project['client_tel'] }}</span>
+            </h4>
+            <a href="{{ route('projects.edit', $project) }}" class="btn btn-primary">Modifica</a>
+        </div>
+    </div>
+
+</div>
+
+</main>
 @endsection
