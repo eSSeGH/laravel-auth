ad32f63924fd54cae9f83bff54a41fb13283c7ec
eSSeGH <ste.virgadaula@gmail.com>
2023-04-25 16:19:54 +0200

    FILE userseeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\ProjectSeeeder;
+use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        $this->call([
+            UserSeeder::class,
             ProjectSeeder::class
        ]);
     }
